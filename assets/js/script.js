d252bcde043b1d9f61e123ab24717ee44f10c4e2
Sudospective <avery@sudospective.net>
2024-10-17 08:41:44 -0700

    Add about page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,55 @@
+let showAbout = false;
+let started = false;
+let startTime = 0;
+let startValue = 0;
+let endValue = 0;
+let currentValue = 0;
+let animID = null;
+
+function easeInOutCirc(x) {
+  return x < 0.5
+    ? (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2
+    : (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
+}
+
+function moveAbout() {
+  if (!started) {
+    startTime = Date.now();
+    currentValue = startValue;
+    started = true;
+  }
+  let time = (Date.now() - startTime) * 2;
+  const mainDoc = document.getElementsByTagName("main")[0];
+  if (time < 1000) {
+    currentValue = startValue + (endValue - startValue) * easeInOutCirc(time * 0.001);
+    console.log(currentValue);
+    mainDoc.style.top = `${currentValue * 100}vh`;
+    mainDoc.style.bottom = `${-currentValue * 100}vh`;
+  }
+  else {
+    currentValue = endValue;
+    mainDoc.style.top = `${endValue * 100}vh`;
+    mainDoc.style.bottom = `${-endValue * 100}vh`;
+    started = false;
+    return;
+  }
+  animID = requestAnimationFrame(moveAbout);
+}
+
+function onAboutClick() {
+  showAbout = true;
+  startValue = 1.1;
+  endValue = 0;
+  moveAbout();
+}
+
+function onAboutDoneClick() {
+  showAbout = false;
+  startValue = 0;
+  endValue = 1.1;
+  moveAbout();
+}
+
 function main() {
   const taglines = [
     "Puzzle Enthusiast",
